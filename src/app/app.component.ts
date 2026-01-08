50d6cd2e4a1250927f74baee56ea5747e7106faf
pzoli <papp.zoltan@integrity.hu>
2026-01-08 20:54:41 +0100

    recording start/stop events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,34 +11,48 @@ import { environment } from '../environments/environment';
 })
 export class AppComponent {
   @ViewChild('video') videoElement!: ElementRef<HTMLVideoElement>;
-  private socket!: WebSocket;
+  private socket: WebSocket | undefined = undefined;
+  public sourceName = environment.SOURCE_NAME;
+  public mediaRecorder: MediaRecorder | undefined = undefined;
+  public stream: MediaStream | undefined = undefined;
 
   ngOnInit() {
+  }
+
+  public startRecording() {
     this.socket = new WebSocket(environment.WEBSOCKET_URL);
     this.socket.onclose = () => console.log('WebSocket connection closed');
-    this.socket.onopen = () => this.startCamera();
+    this.socket.onopen = () => this.socket!.send('[source]:'+this.sourceName); this.startCamera();
     this.socket.onerror = (error) => console.error('WebSocket error:', error);
   }
 
+  public stopRecording() {
+    this.mediaRecorder!.stop();
+    this.stream!.getTracks().forEach(track => track.stop());
+    this.socket!.close();
+    this.mediaRecorder = undefined;
+    this.stream = undefined;
+  }
+
   async startCamera() {
-    const stream = await navigator.mediaDevices.getUserMedia({ 
+    this.stream = await navigator.mediaDevices.getUserMedia({ 
       video: { width: 800, height: 600, frameRate: 20 } , audio: true
     });
-    this.videoElement.nativeElement.srcObject = stream;
+    this.videoElement.nativeElement.srcObject = this.stream;
 
     const options = {
       mimeType: 'video/webm;codecs=vp8,opus',
       //videoBitsPerSecond: 500000
     };
 
-    const mediaRecorder = new MediaRecorder(stream, options);
+    this.mediaRecorder = new MediaRecorder(this.stream, options);
 
-    mediaRecorder.ondataavailable = async (event) => {
-      if (event.data.size > 0 && this.socket.readyState === WebSocket.OPEN) {
+    this.mediaRecorder.ondataavailable = async (event) => {
+      if (event.data.size > 0 && this.socket != undefined && this.socket!.readyState === WebSocket.OPEN) {
         this.socket.send(event.data);
       }
     };
 
-    mediaRecorder.start(1000); 
+    this.mediaRecorder.start(1000); 
   }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    WEBSOCKET_URL: 'wss://192.168.1.161:8443/video-stream'
+    WEBSOCKET_URL: 'wss://192.168.1.161:8443/video-stream',
+    SOURCE_NAME:'lenovo-camera'
 };
